156a243e8fd33b8c0bcdbb6be3e3ba5ec3b6cf02
danieltdocarmo <danielt.docarmo@gmail.com>
2025-02-02 14:57:44 -0300

    first commit

---
+++ src/test/java/com/codespace/codespace/CodespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codespace.codespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
